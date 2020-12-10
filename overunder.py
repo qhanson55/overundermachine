37718c026a74e34d8e222bfd293b76f5b8719ff5
Quinn Hanson <quinnhanson55@gmail.com>
2020-12-10 11:11:45 -0800

    changed scraper so program runs faster, added more return values

--- overunder.py
+++ overunder.py
@@ -2,11 +2,50 @@ import overunder_scraper
 import sys
 
 total_games = 0
+total_points_for = 0
+total_points_against = 0
 over_games = 0
 under_games = 0
 even_games = 0
-# wins = 0
-# losses = 0
+wins_over = 0
+losses_over = 0
+wins_under = 0
+losses_under = 0
+
+team_abrevs = {
+    'arizona cardinals': 'crd',
+    'atlanta falcons': 'atl',
+    'baltimore ravens': 'rav',
+    'buffalo bills': 'buf',
+    'carolina panthers': 'car',
+    'chicago bears': 'chi',
+    'cincinnati bengals': 'cin',
+    'cleveland browns': 'cle',
+    'dallas cowboys': 'dal',
+    'denver broncos': 'den',
+    'detroit lions': 'det',
+    'green bay packers': 'gnb',
+    'houston texans': 'htx',
+    'indianapolis colts': 'clt',
+    'jacksonville jaguars': 'jax',
+    'kansas city chiefs': 'kan',
+    'las vegas raiders': 'rai',
+    'los angeles chargers': 'sdg',
+    'los angeles rams': 'ram',
+    'miami dolphins': 'mia',
+    'minnesota vikings': 'min',
+    'new england patriots': 'nwe',
+    'new orleans saints': 'nor',
+    'new york giants': 'nyg',
+    'new york jets': 'nyj',
+    'philadelphia eagles': 'phi',
+    'pittsburgh steelers': 'pit',
+    'san francisco 49ers': 'sfo',
+    'seattle seahawks': 'sea',
+    'tampa bay buccaneers': 'tam',
+    'tennesse titans': 'oti',
+    'washington football team': 'was'
+}
 
 # parse arguments
 for i in range(len(sys.argv)):
@@ -14,31 +53,42 @@ for i in range(len(sys.argv)):
         continue
     elif (i == 1):
         year = sys.argv[i]
+    # elif (i == 2):
+    #     week = int(sys.argv[i])
     elif (i == 2):
-        week = int(sys.argv[i])
-    elif (i == 3):
         score = float(sys.argv[i])    
-    elif (i == 4):
+    elif (i == 3):
         team = sys.argv[i].lower()
-    elif (i > 4):
-        team = team + ' ' + sys.argv[i].lower()    
+    elif (i > 3):
+        team = team + ' ' + sys.argv[i].lower()       
 
-for i in range(week - 1):
-    games = overunder_scraper.run_scraper(str(year), str(week - 1 - i))
+team = team_abrevs[team]
 
-    for game in games:
-        game_winner = game.winner.lower()
-        game_loser = game.loser.lower()
-        game_score = float(game.total_score)
+# for i in range(week - 1):
+games = overunder_scraper.run_scraper(team, year)
 
-        if (game_winner == team or game_loser == team):
-            total_games += 1
-            if (game_score > score):
-                over_games += 1
-            elif (game_score < score):
-                under_games += 1
-            elif (game_score == score):
-                even_games += 1
+total_games = len(games)
+
+for game in games:
+    # game_winner = game.winner.lower()
+    # game_loser = game.loser.lower()
+    total_points_for = total_points_for + game.score_for
+    total_points_against = total_points_against + game.score_against
+    game_score = float(game.total_score)
+    if (game_score > score):
+        over_games += 1
+        if (game.winner):
+            wins_over += 1
+        else:
+            losses_over += 1
+    elif (game_score < score):
+        under_games += 1
+        if (game.winner):
+            wins_under += 1
+        else:
+            losses_under += 1
+    elif (game_score == score):
+        even_games += 1
             # elif (game_loser == team):
             #     losses += 1
             # else:
@@ -50,5 +100,9 @@ print(team.upper() + ':')
 print('Has ' + str(over_games) + ' out of ' + str(total_games) + ' over a score of ' + str(score) + '.')
 print('Has ' + str(under_games) + ' out of ' + str(total_games) + ' under a score of ' + str(score) + '.')
 print('Has ' + str(even_games) + ' out of ' + str(total_games) + ' even with a score of ' + str(score) + '.')
-# print('Was winner of ' + str(wins) + ' games')
-# print('Was loser of ' + str(losses) + ' games')
+print('Was winner of ' + str(wins_over) + ' over games')
+print('Was loser of ' + str(losses_over) + ' over games')
+print('Was winner of ' + str(wins_under) + ' under games')
+print('Was loser of ' + str(losses_under) + ' under games')
+print("Average points scored = " + str(total_points_for / total_games))
+print("Average points against = " + str(total_points_against / total_games))
--- overunder_scraper.py
+++ overunder_scraper.py
@@ -2,39 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 
 class Game:
-    def __init__(self, winner, loser, total_score):
+    def __init__(self, winner, score_for, score_against, total_score):
         self.winner = winner
-        self.loser = loser
+        self.score_for = score_for
+        self.score_against = score_against
         self.total_score = total_score
 
-def run_scraper(year, week):
-    URL = 'https://www.pro-football-reference.com/years/'+ year + '/week_' + week + '.htm'
+def run_scraper(team, year):
+    URL = 'https://www.pro-football-reference.com/teams/'+ team + '/' + year + '.htm'
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    results = soup.find(class_='game_summaries')
+    results = soup.find('table', id='games')
 
-    games_soup = results.find_all('table', class_='teams')
+    games_soup = results.find('tbody')
+    games_soup = games_soup.find_all('tr')
     games_objs = []
 
     for game_soup in games_soup:
-        winner = game_soup.find('tr', class_='winner')
-        loser = game_soup.find('tr', class_='loser')
-        if (winner is None) or (loser is None):
+        # print(game_soup, end='\n')
+        winner = game_soup.find('td', {'data-stat':'game_outcome'}).text
+        # loser = game_soup.find('tr', class_='loser')
+        # draw = game_soup.find('tr', class_='draw')
+
+        if (winner is None):
             continue
 
-        winner_score = winner.find(class_='right').text
-        loser_score = loser.find(class_='right').text
-        if (winner_score is None) or (loser_score is None):
+        team_score = game_soup.find('td', {'data-stat':'pts_off'}).text
+        opp_score = game_soup.find('td', {'data-stat':'pts_def'}).text
+        if (team_score is None) or (opp_score is None) or (team_score == ''):
             continue
-        total_score = int(winner_score) + int(loser_score)
+        total_score = int(team_score) + int(opp_score)
 
-        winner = winner.find('a').text
-        loser = loser.find('a').text
-        if (winner is None) or (loser is None):
-            continue
+        # winner = winner.find('a').text
+        # loser = loser.find('a').text
+        # if (winner is None) or (loser is None):
+        #     continue
 
-        game_obj = Game(winner, loser, total_score)
+        game_obj = Game((winner == 'W'), int(team_score), int(opp_score), total_score)
         games_objs.append(game_obj)
 
     return games_objs
