8ea549c1bcc5ad747dcd7446a52070baff1cb47d
Quinn Hanson <quinnhanson55@gmail.com>
2020-12-09 22:27:55 -0800

    first version, just counting number number of overs/unders

--- overunder.py
+++ overunder.py
@@ -1,1 +1,54 @@
+import overunder_scraper
+import sys
 
+total_games = 0
+over_games = 0
+under_games = 0
+even_games = 0
+# wins = 0
+# losses = 0
+
+# parse arguments
+for i in range(len(sys.argv)):
+    if (i == 0):
+        continue
+    elif (i == 1):
+        year = sys.argv[i]
+    elif (i == 2):
+        week = int(sys.argv[i])
+    elif (i == 3):
+        score = float(sys.argv[i])    
+    elif (i == 4):
+        team = sys.argv[i].lower()
+    elif (i > 4):
+        team = team + ' ' + sys.argv[i].lower()    
+
+for i in range(week - 1):
+    games = overunder_scraper.run_scraper(str(year), str(week - 1 - i))
+
+    for game in games:
+        game_winner = game.winner.lower()
+        game_loser = game.loser.lower()
+        game_score = float(game.total_score)
+
+        if (game_winner == team or game_loser == team):
+            total_games += 1
+            if (game_score > score):
+                over_games += 1
+            elif (game_score < score):
+                under_games += 1
+            elif (game_score == score):
+                even_games += 1
+            # elif (game_loser == team):
+            #     losses += 1
+            # else:
+            #     wins += 1
+
+
+
+print(team.upper() + ':')
+print('Has ' + str(over_games) + ' out of ' + str(total_games) + ' over a score of ' + str(score) + '.')
+print('Has ' + str(under_games) + ' out of ' + str(total_games) + ' under a score of ' + str(score) + '.')
+print('Has ' + str(even_games) + ' out of ' + str(total_games) + ' even with a score of ' + str(score) + '.')
+# print('Was winner of ' + str(wins) + ' games')
+# print('Was loser of ' + str(losses) + ' games')
--- overunder_scraper.py
+++ overunder_scraper.py
@@ -1,1 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
 
+class Game:
+    def __init__(self, winner, loser, total_score):
+        self.winner = winner
+        self.loser = loser
+        self.total_score = total_score
+
+def run_scraper(year, week):
+    URL = 'https://www.pro-football-reference.com/years/'+ year + '/week_' + week + '.htm'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_='game_summaries')
+
+    games_soup = results.find_all('table', class_='teams')
+    games_objs = []
+
+    for game_soup in games_soup:
+        winner = game_soup.find('tr', class_='winner')
+        loser = game_soup.find('tr', class_='loser')
+        if (winner is None) or (loser is None):
+            continue
+
+        winner_score = winner.find(class_='right').text
+        loser_score = loser.find(class_='right').text
+        if (winner_score is None) or (loser_score is None):
+            continue
+        total_score = int(winner_score) + int(loser_score)
+
+        winner = winner.find('a').text
+        loser = loser.find('a').text
+        if (winner is None) or (loser is None):
+            continue
+
+        game_obj = Game(winner, loser, total_score)
+        games_objs.append(game_obj)
+
+    return games_objs
